ba2e1abc8ba3a2f76f2ff6ed70f4580b347a33a7
BrMD <dorigon.matheus8@gmail.com>
2023-09-12 09:55:24 -0300

    login screen implementing react-router

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -22,11 +22,26 @@ function Form() {
   return (
     <Container>
       <FormLogin onSubmit={handleSubmit(onSubmit)}>
-        <InputField {...register("login")} placeholder="Login" />
-        {errors.login && <p>{errors.login.message}</p>}
-        <InputField {...register("senha")} placeholder="senha" />
-        {errors.senha && <p>{errors.senha.message}</p>}
-        <InputField type="submit" />
+        <DivForm>
+          <InputField
+            className={`${errors.login ? "errorFormColor" : "normalFormColor"}`}
+            {...register("login")}
+            placeholder="Login"
+          />
+          {errors.login && <p>{errors.login.message}</p>}
+        </DivForm>
+        <DivForm>
+          <InputField
+            className={`${errors.senha ? "errorFormColor" : "normalFormColor"}`}
+            {...register("senha")}
+            placeholder="Senha"
+          />
+          {errors.senha && <p>{errors.senha.message}</p>}
+        </DivForm>
+        <SpanCreateAccount>
+          You dont have an account, create one <a href="#">Here!</a>
+        </SpanCreateAccount>
+        <ButtonForm type="submit">Enviar</ButtonForm>
       </FormLogin>
     </Container>
   );
@@ -34,20 +49,63 @@ function Form() {
 
 const Container = styled.div`
   height: 100vh;
-  width: 100%;
+  width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 const FormLogin = styled.form`
+  width: 80%;
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 15px;
 `;
-
+const DivForm = styled.div`
+  font-family: "Hind", sans-serif;
+  height: 5.5em;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  && p {
+    margin: 0.3em 0 0 0;
+    color: var(--red--light-);
+  }
+`;
 const InputField = styled.input`
-  width: 10em;
+  font-size: 15px;
+  border: 2px solid;
+  border-radius: 12px;
+  padding: 0.7em;
+  width: 14em;
   height: 2em;
+  transition: all 0.5s;
+  outline: none;
+  &&:focus {
+    border-color: var(--aquamarine-);
+    color: #000;
+  }
+`;
+const ButtonForm = styled.button`
+  width: 8.5em;
+  height: 2.5em;
+  cursor: pointer;
+  color: #fff;
+  border-radius: 10px;
+  border: none;
+  background: var(--green--dark-);
+  transition: all 0.5s;
+  &&:hover {
+    background: #41fa7e;
+  }
+`;
+const SpanCreateAccount = styled.span`
+  margin: 0 0 1em 0;
+  font-size: 0.9em;
+  font-weight: 400;
+  font-family: "Mukta", sans-serif;
+  && > a {
+    text-decoration: none;
+    color: var(--aquamarine-);
+  }
 `;
 export default Form;
