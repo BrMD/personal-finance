e5e8632af6f844e9e53fc87fc55d96e56d837bd5
BrMD <dorigon.matheus8@gmail.com>
2023-09-14 11:36:33 -0300

    ending dashboard view

--- src/pages/DashBoard.tsx
+++ src/pages/DashBoard.tsx
@@ -9,12 +9,51 @@ const DashBoard = () => {
       <ContainerDashboard>
         <DashboardDiv>
           <SpanDash>Dashboard</SpanDash>
-          <DivTesteBorder></DivTesteBorder>
+          <DivDashboard>
+            <DashboardViewDiv className="saldoAtual">
+              <DivSpanImg>
+                <span>Saldo Atual</span>
+                <img src="bank.png" width={"20px"} height={"20px"} />
+              </DivSpanImg>
+              <span>R$ 5.000,00</span>
+            </DashboardViewDiv>
+            <DashboardViewDiv className="receitas">
+              <DivSpanImg>
+                <span>Receitas</span>
+                <img src="chart_ascending.png" width={"20px"} height={"20px"} />
+              </DivSpanImg>
+              <span>R$ 5.000,00</span>
+            </DashboardViewDiv>
+            <DashboardViewDiv className="despesas">
+              <DivSpanImg>
+                <span>Saldo Atual</span>
+                <img
+                  src="chart_descending.png"
+                  width={"20px"}
+                  height={"20px"}
+                />
+              </DivSpanImg>
+              <span>R$ 5.000,00</span>
+            </DashboardViewDiv>
+            <DashboardViewDiv className="cartaoDeCredito">
+              <DivSpanImg>
+                <span>Cartao de Credito</span>
+                <img src="credit_card.png" width={"20px"} height={"20px"} />
+              </DivSpanImg>
+              <span>R$ 5.000,00</span>
+            </DashboardViewDiv>
+          </DivDashboard>
         </DashboardDiv>
       </ContainerDashboard>
     </>
   );
 };
+
+const DivSpanImg = styled.div`
+  display: flex;
+  gap: 7px;
+  align-items: center;
+`;
 
 const DashboardDiv = styled.div`
   font-family: "Lato", sans-serif;
@@ -22,17 +61,32 @@ const DashboardDiv = styled.div`
   height: 94%;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  align-items: center;
   background-color: #f2f2f2;
   border-radius: 25px;
   border: 2px solid var(--tropical--indigo-);
 `;
-const DivTesteBorder = styled.div`
-  clip-path: polygon(16% 91%, 100% 91%, 83% 100%, 0 100%);
-  height: 2em;
-  width: 10em;
-  background-color: #000;
+
+const DashboardViewDiv = styled.div`
+  font-family: "Mukta", sans-serif;
+  font-weight: 500;
+  cursor: pointer;
+  width: 12em;
+  height: 4em;
+  margin: 2.5em;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  border-radius: 12px;
 `;
+
+const DivDashboard = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
+
 const ContainerDashboard = styled.div`
   display: flex;
   flex-direction: row;
@@ -41,11 +95,14 @@ const ContainerDashboard = styled.div`
   justify-content: center;
   align-items: center;
 `;
+
 const SpanDash = styled.span`
   margin-top: 2em;
   height: 1.2em;
+  width: auto;
   font-size: 30px;
   font-weight: 400;
   border-bottom: 2px solid var(--aquamarine-);
 `;
+
 export default DashBoard;
